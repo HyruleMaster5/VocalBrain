dcec3e05275af0c7e21991186c010fd6e9cea4cd
Christian Hyrule-Cardinal <christo_567@hotmail.com>
2016-01-23 17:56:35 -0500

    More ColorTrap shit

---
+++ VocalBrain/app/src/main/java/com/canyourunit/vocalbrain/activities/colortrap/ColorTrapScoreActivity.java
@@ -0,0 +1,15 @@
+/***************************************************************************************************
+ * Score activity of the minigame Color Trap
+ *
+ * @author: Christian Hardy-Cardinal
+ * @since: 1.0.0 2016-01-23
+ * *************************************************************************************************
+ * Date       Author Description
+ * ---------- ------ -------------------------------------------------------------------------------
+ */
+package com.canyourunit.vocalbrain.activities.colortrap;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class ColorTrapScoreActivity extends AppCompatActivity{
+}
